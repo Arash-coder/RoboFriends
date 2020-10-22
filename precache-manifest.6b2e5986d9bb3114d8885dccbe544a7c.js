bb8903584c5d1d678cfa03d639a64a35b8fe5e42
Arash Barghash <arash.barghash138014@gmail.com>
2020-10-22 13:00:40 +0330

    Updates

---
+++ precache-manifest.6b2e5986d9bb3114d8885dccbe544a7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589fcdefe2dfe7008b054b3f7e829f0a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "51276ed9e717ef08ec48",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d92f4215456447800355",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "51276ed9e717ef08ec48",
+    "url": "/robofriends/static/js/2.a687f2ae.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/robofriends/static/js/2.a687f2ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d92f4215456447800355",
+    "url": "/robofriends/static/js/main.79aaac0e.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
