ba75ca5794ebc29a629f0c4a61e062fc8f5a2ee3
Arash Barghash <arash.barghash138014@gmail.com>
2020-10-22 12:54:49 +0330

    RoboFriends

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import CardList from './CardList';
+import {robots} from './robots'
+import Searchbox from './Searchbox';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    constructor(){
+        super()
+        this.state ={
+            robots:[],
+            searchfield: ''
+        }
+    }
+    componentDidMount(){
+        this.setState({
+            robots:robots
+        })
+    }
+    onsearchchange =(event) =>{
+        this.setState({searchfield:event.target.value})
+        const filteredrobots = this.state.robots.filter(robots=>{
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+        console.log(filteredrobots);
+    }
+    render() {
+        const filteredrobots = this.state.robots.filter(robots=>{
+        return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+        return(
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <Searchbox searchchange={this.onsearchchange}/>
+                <CardList robots={filteredrobots} />
+            </div>
+        );
+    }
 }
+
+
 
 export default App;
